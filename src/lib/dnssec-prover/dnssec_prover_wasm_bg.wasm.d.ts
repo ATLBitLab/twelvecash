de2df8bf34035fe8988687eb56ffdb9c0d2aff80
Stephen DeLorme <stephen@d.elor.me>
2024-04-23 09:43:49 -0400

    added dnssec prover lib files

---
+++ src/lib/dnssec-prover/dnssec_prover_wasm_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_wasmproofbuilder_free(a: number): void;
+export function init_proof_builder(a: number, b: number, c: number): number;
+export function process_query_response(a: number, b: number, c: number): void;
+export function get_next_query(a: number, b: number): void;
+export function get_unverified_proof(a: number, b: number): void;
+export function verify_byte_stream(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
