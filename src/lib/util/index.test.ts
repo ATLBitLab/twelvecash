697c4056fc200b749d22e80a737628d397a20577
Chad Welch <cwelc001@gmail.com>
2024-07-13 17:28:47 -0400

    add basic unit test

---
+++ src/lib/util/index.test.ts
@@ -0,0 +1,11 @@
+// import { lnaddrToLNURL } from "./index";
+const lnaddrToLNURL = require("./index").lnaddrToLNURL;
+
+test("using different lightning address inputs", () => {
+  expect(lnaddrToLNURL("chad@strike.me")).toBe(
+    "lnurl1dp68gurn8ghj7um5wf5kkefwd4jj7tnhv4kxctttdehhwm30d3h82unvwqhkx6rpvsclqksp"
+  );
+  expect(() => lnaddrToLNURL("")).toThrow();
+  expect(() => lnaddrToLNURL("test")).toThrow();
+  expect(() => lnaddrToLNURL("@domain.com")).toThrow();
+});
--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -2,9 +2,9 @@ import { bech32 } from "bech32";
 
 export const lnaddrToLNURL = (lnaddr: string) => {
   const [username, domain] = lnaddr.split("@");
-  // TODO: Fix checks
   if (!username || !domain)
     throw new Error("Failed to parse lightning address");
+
   const decodedLnurl = `https://${domain}/.well-known/lnurlp/${username}`;
   console.debug("decodedLnurl", decodedLnurl);
 
