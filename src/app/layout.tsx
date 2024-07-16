74b66c963ef500d2a164c8f211c9576c9969e0db
Stephen DeLorme <stephen@d.elor.me>
2024-07-15 22:24:12 -0400

    new bg gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="text-purple-800 bg-12teal h-full flex flex-col justify-between">
+    <html lang="en" className="p-0 m-0">
+      <body className={inter.className + " p-0 m-0"}>
+        <div className="text-purple-800 bg-12teal bg-gradient-to-b from-purple-800/20 from-0% to-purple-800/0 to-50% h-full flex flex-col justify-between">
           {children}
           <footer className="border-t border-t-purple-800 pt-2 mt-4 text-purple-800">
             <ul className="flex flex-col gap-2 md:flex-row md:gap-4 md: p-4">
