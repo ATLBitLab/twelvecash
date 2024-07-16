49173b84a992ee82b9c0865b09064d4282a6c317
Stephen DeLorme <stephen@d.elor.me>
2024-07-16 07:13:50 -0400

    twelvecash logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Urbanist } from 'next/font/google'
 import './globals.css'
+import TwelveCashLogo from './components/TwelveCashLogo'
 
 const inter = Inter({ subsets: ['latin'] })
+const urbanist = Urbanist({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Twelve Cash',
@@ -16,8 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="p-0 m-0">
-      <body className={inter.className + " p-0 m-0"}>
+      <body className={urbanist.className + " p-0 m-0"}>
         <div className="text-purple-800 bg-12teal bg-gradient-to-b from-purple-800/20 from-0% to-purple-800/0 to-50% h-full flex flex-col justify-between">
+          <header className="border-b border-b-white/40 p-5">
+            <TwelveCashLogo />
+            Check
+            New
+          </header>
           {children}
           <footer className="border-t border-t-purple-800 pt-2 mt-4 text-purple-800">
             <ul className="flex flex-col gap-2 md:flex-row md:gap-4 md: p-4">
