311d4681cb97cf41d5d453c8535fed9a33aa8a73
Chad Welch <cwelc001@gmail.com>
2024-07-16 08:42:42 -0400

    use period separator

--- src/app/v2/record/route.ts
+++ src/app/v2/record/route.ts
@@ -85,6 +85,7 @@ export async function POST(req: NextRequest) {
       userName = uniqueNamesGenerator({
         dictionaries: [adjectives, animals],
         length: 2,
+        separator: ".",
       });
     }
 
