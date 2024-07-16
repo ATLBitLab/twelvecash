a2eaa9a32671dfda1bb3c1cf254daadb5dee35ea
Stephen DeLorme <stephen@d.elor.me>
2024-07-16 08:13:50 -0400

    fancy buttons

--- src/app/components/TwelveCashLogo.tsx
+++ src/app/components/TwelveCashLogo.tsx
@@ -1,8 +1,8 @@
 export default function TwelveCashLogo() {
     return(
         <>
-            <a href="/" className="text-2xl bg-purple-800 bg-gradient-to-br from-[#00F0FF]/40 to-[#00F0FF]/0 inline-block rounded-full text-white tracking-tight font-light no-underline overflow-hidden shadow-lg">
-                <span className="px-5 py-3 inline-block bg-white/0 hover:bg-white/10 transition-all duration-200">Twelve Cash</span>
+            <a href="/" className="text-2xl bg-purple-800 bg-gradient-to-br from-[#00F0FF]/40 to-[#00F0FF]/0 inline-block rounded-full text-white tracking-tight font-light no-underline overflow-hidden shadow-lg flex-none">
+                <span className="px-5 py-3 inline-block bg-white/0 hover:bg-white/10 transition-all duration-200 h-fu">Twelve&nbsp;Cash</span>
             </a>
         </>
     )
