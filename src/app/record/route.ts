aae06f3eed17e405545367fcb5eab1051705f9aa
Chad Welch <cwelc001@gmail.com>
2024-07-16 11:10:20 -0400

    fix duplicate txt record

--- src/app/record/route.ts
+++ src/app/record/route.ts
@@ -1,8 +1,5 @@
-import { Content } from "next/font/google";
 import { NextRequest, NextResponse } from "next/server";
 const axios = require("axios").default;
-
-
 
 export async function POST(req: NextRequest) {
   let localPart: string, bolt12: string;
@@ -27,106 +24,69 @@ export async function POST(req: NextRequest) {
 
   // Begin assembling DNS name
   const fullName = process.env.NETWORK
-      ? localPart + ".user._bitcoin-payment." + process.env.NETWORK
-      : localPart + ".user._bitcoin-payment";
+    ? localPart + ".user._bitcoin-payment." + process.env.NETWORK
+    : localPart + ".user._bitcoin-payment";
 
-  // Digital Ocean
-  if(process.env.PROVIDER !== "cloudflare") {
-    const DO_URL = `https://api.digitalocean.com/v2/domains/${process.env.DOMAIN}/records`;
-    const config = {
-      headers: { Authorization: `Bearer ${process.env.DO_TOKEN}` },
-    };
-
-    
-    const queryParams = `?type=TXT&name=${fullName}.${process.env.DOMAIN}`;
-    const query = `${DO_URL}${queryParams}`;
-
-    try {
-      const res = await axios.get(query, config);
-      console.debug("query res.data", res.data);
-      console.debug("query res.data.domain_records", res.data.domain_records);
-      if (
-        res.data.domain_records.length !== 0 &&
-        res.data.domain_records[0].name === localPart
-      ) {
-        return NextResponse.json({ message: "Name is taken." }, { status: 409 });
-      }
-    } catch (error: any) {
-      console.error("error", error);
-      return NextResponse.json(
-        {
-          message: "Failed to create Paycode.",
+  // First do a DoH lookup to see if a TXT record exists for the given name
+  try {
+    const res = await axios.get(
+      `https://cloudflare-dns.com/dns-query?name=${fullName}.${process.env.DOMAIN}&type=txt`,
+      {
+        headers: {
+          Accept: "application/dns-json",
         },
-        { status: 400 }
-      );
-    }
-
-    const data = {
-      type: "TXT",
-      name: fullName,
-      data: "bitcoin:?lno=" + bolt12,
-      priority: null,
-      port: null,
-      ttl: 1800,
-      weight: null,
-      flags: null,
-      tag: null,
-    };
-
-    try {
-      const res = await axios.post(DO_URL, data, config);
-      console.debug(res.data);
-    } catch (error: any) {
+      }
+    );
+    // Answer is an array if there are records, undefined otherwise
+    if (res.data.Answer)
       return NextResponse.json(
-        { message: "Failed to create Paycode." },
-        { status: 400 }
+        { message: "Name is already taken." },
+        { status: 409 }
       );
-    }
-
+  } catch (e: any) {
     return NextResponse.json(
-      { message: "Bolt12 Address Created" },
-      { status: 201 }
+      { message: "Failed to lookup paycode." },
+      { status: 400 }
     );
   }
-  // Cloudflare
-  else {
-    const CF_URL = `https://api.cloudflare.com/client/v4/zones/${process.env.CF_DOMAIN_ID}/dns_records/`;
-    
-    const config = {
-      method: "POST",
-      headers: {
-        Content_Type: "application/json",
-        Authorization: `Bearer ${process.env.CF_TOKEN}`
-      }
-    };
 
-    const data = {
-      content: "bitcoin:?lno=" + bolt12,
-      name: `${fullName}.${process.env.DOMAIN}`,
-      proxied: false,
-      type: "TXT",
-      comment: "Twelve Cash User DNS Update",
-      ttl: 3600
-    };
+  const CF_URL = `https://api.cloudflare.com/client/v4/zones/${process.env.CF_DOMAIN_ID}/dns_records/`;
 
-    try {
-      const res = await axios.post(CF_URL, data, config);
-      console.debug(res.data);
-    } catch (error: any) {
-      let message = "Failed to create Paycode."
-      if(error.response.data.errors[0].code === 81058) {
-          message = "Name is already taken."
-      }
+  const config = {
+    method: "POST",
+    headers: {
+      Content_Type: "application/json",
+      Authorization: `Bearer ${process.env.CF_TOKEN}`,
+    },
+  };
+
+  const data = {
+    content: "bitcoin:?lno=" + bolt12,
+    name: `${fullName}.${process.env.DOMAIN}`,
+    proxied: false,
+    type: "TXT",
+    comment: "Twelve Cash User DNS Update",
+    ttl: 3600,
+  };
+
+  try {
+    const res = await axios.post(CF_URL, data, config);
+    console.debug(res.data);
+  } catch (error: any) {
+    if (error.response.data.errors[0].code === 81058) {
       return NextResponse.json(
-        
-        { message: message },
-        { status: 400 }
+        { message: "Name is already taken." },
+        { status: 409 }
       );
     }
-
     return NextResponse.json(
-      { message: "Bolt12 Address Created" },
-      { status: 201 }
+      { message: "Failed to create Paycode." },
+      { status: 400 }
     );
   }
+
+  return NextResponse.json(
+    { message: "Bolt12 Address Created" },
+    { status: 201 }
+  );
 }
