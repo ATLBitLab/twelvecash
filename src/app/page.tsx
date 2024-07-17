8018f009c0ab8fc7ea783c2c18ef5058ede0a931
Stephen DeLorme <stephen@d.elor.me>
2024-07-17 17:13:19 -0400

    moving fancy letters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,20 @@ import { ArrowRightIcon, SearchIcon } from "@bitcoin-design/bitcoin-icons-react/
 
 export default function Home() {
   const defaultDomain = process.env.DOMAIN ? process.env.DOMAIN : "twelve.cash";
+  const users = [
+    {user:'sensible.pangolin', domain: defaultDomain},
+    {user:'magnificent.deinonychus', domain: defaultDomain},
+    {user:'whimsical.salamander', domain: defaultDomain},
+    {user:'rowdy.archaeopteryx', domain: defaultDomain},
+    {user:'insidious.mongoose', domain: defaultDomain},
+    {user:'formidable.mastodon', domain: defaultDomain},
+  ]
   return (
     <main className="mx-auto max-w-4xl flex flex-col gap-9 w-full text-center p-6">
       <div>
         <TwelveCashLogo size="large" />
       </div>
-      <Bip353Box user={'sensible.pangolin'} domain={defaultDomain} />
+      <Bip353Box users={users} />
       <h1 className="max-w-xl mx-auto">A simple way to share your bitcoin payment info with the world.</h1>
       <div className="max-w-md mx-auto flex flex-col gap-6">
         <Button href="/new" size="large">Make a Pay Code <ArrowRightIcon className="w-6 h-6" /></Button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,13 +17,41 @@ const config: Config = {
         '12teal': '#0FFFC5'
       },
       animation: {
-        'loading-pulse': 'loading-pulse 1s linear infinite'
+        'loading-pulse': 'loading-pulse 1s linear infinite',
+        'slide-up-1': 'slide-up 18s ease-in-out infinite both',
+        'slide-up-2': 'slide-up 18s ease-in-out infinite both 3s',
+        'slide-up-3': 'slide-up 18s ease-in-out infinite both 6s',
+        'slide-up-4': 'slide-up 18s ease-in-out infinite both 9s',
+        'slide-up-5': 'slide-up 18s ease-in-out infinite both 12s',
+        'slide-up-6': 'slide-up 18s ease-in-out infinite both 15s',
       },
       keyframes: {
         'loading-pulse': {
           '0%': {transform: 'scaleX(0.5) translateX(-100%)'},
           '100%': {transform: 'scaleX(0.5) translateX(200%)'},
-        }
+        },
+        'slide-up': {
+          '0%': {
+            opacity: '0.0',
+            transform: 'translateY(100%)',
+          },
+          '2%': {
+            opacity: '1.0',
+            transform: 'translateY(0%)',
+          },
+          '14%': {
+            opacity: '1.0',
+            transform: 'translateY(0%)',
+          },
+          '16%': {
+            opacity: '0.0',
+            transform: 'translateY(-100%)',
+          },
+          '100%': {
+            opacity: '0.0',
+            transform: 'translateY(-100%)',
+          },
+        },
       },
     },
   },
