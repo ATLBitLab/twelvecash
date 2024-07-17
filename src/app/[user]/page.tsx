fa370eb39342dcfbb6ab0488d6541738e9e6fbcd
Stephen DeLorme <stephen@d.elor.me>
2024-07-16 23:58:31 -0400

    validation

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -3,26 +3,25 @@ import Bip353Box from '../components/Bip353Box';
 import CopyUserLinkButton from '../features/CopyUserLinkButton';
 import CopyBip353Button from '../features/CopyBip353Button';
 import PaymentDetail from '../components/PaymentDetail';
+import Button from '../components/Button';
+import { ArrowLeftIcon } from '@bitcoin-design/bitcoin-icons-react/filled';
+import UserDetails from '../features/UserDetails';
 
 export default function User({params}: {params: {user: string}}) {
   const decoded = decodeURIComponent(params.user);
   const [user, domain] = decoded.split('@');
   return (
     <main className="mx-auto max-w-4xl flex flex-col gap-9 w-full text-center p-6 min-h-full">
-        <h1>Check Paycode</h1>
-        <Bip353Box user={user} domain={domain} />
-        Validated! (Or not)
-        <div className="flex flex-row gap-4">
-          <CopyUserLinkButton link={'https://twelve.cash/' + user + '@' + domain} />
-          <CopyBip353Button bip353={user + '@' + domain} />
-        </div>
-        <div className="flex flex-col gap-4">
+        <UserDetails user={user} domain={domain} />
+        <div className="flex flex-col gap-4 pt-8">
           <h2 className="h3 text-left">Payment Details</h2>
           <dl className="flex flex-col gap-4">
             <PaymentDetail label="All Data" value="bitcoin:bc1pdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfg" uri="bitcoin:bc1p123xyz" />
             <PaymentDetail label="Offer" value="lno1234567890" uri="bitcoin:bc1p123xyz" />
           </dl>
         </div>
+        <hr className="border-purple-800/50" />
+          <Button href="/search" format="outline" size="large"><ArrowLeftIcon className="w-6 h-6" />Search Again</Button>
       </main>
   );
 }
--- src/app/components/PaymentDetail.tsx
+++ src/app/components/PaymentDetail.tsx
@@ -11,7 +11,7 @@ type PaymentDetailProps = {
 export default function PaymentDetail(props:PaymentDetailProps){
     return(
         <>
-            <div className="w-full bg-sky-700 text-white rounded-xl p-6 flex flex-row items-center gap-6 text-4xl font-light">
+            <div className="w-full bg-sky-700 text-white rounded-xl p-6 flex flex-row items-center gap-6 text-4xl font-light drop-shadow-md">
                 <dt className="text-orange-200 flex-none text-left">{props.label}</dt>
                 <dd className="grow text-left whitespace-nowrap text-ellipsis overflow-hidden max-w-[320px]"><a href={props.uri}>{props.value}</a></dd>
                 <span className="flex flex-row justify-end grow">
